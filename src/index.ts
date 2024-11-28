233aeb273ee105f612b44568b563acb23940b19b
aaakash06 <akashbagale182@gmail.com>
2024-11-28 06:20:34 +0545

    crawlee and index.ts

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { CheerioCrawler } from "crawlee";
+
+export const scrapeNext = async () => {
+  try {
+    const nepseUrl = "https://www.sharesansar.com/";
+    let numericValue: number = 0;
+    const crawler = new CheerioCrawler({
+      async requestHandler({ request, $, _, log }) {
+        const nepseValue = $("#tag-id").text().trim();
+        numericValue = parseFloat(nepseValue.replace(/,/g, ""));
+      },
+    });
+
+    await crawler.run([nepseUrl]);
+    console.log(numericValue);
+  } catch (error) {
+    console.error("Error scraping NEPSE index:", error);
+  }
+};
+
+scrapeNext();
