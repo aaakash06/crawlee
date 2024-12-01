193b2066b1773aeafdcb9218fd2fb21631fbf99d
aaakash06 <akashbagale182@gmail.com>
2024-12-01 23:10:26 +0545

    parsing data

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,22 @@ import { PlaywrightCrawler } from "crawlee";
 export const scrapeNext = async () => {
   try {
     const nepseUrl = "https://merolagani.com/latestmarket.aspx";
-    let numericValue: number = 0;
+    let nepseValue: number = 0;
+    let percentChange: number = 0;
     const crawler = new PlaywrightCrawler({
       async requestHandler({ request, page, enqueueLinks, log }) {
-        // const nepseValue = $(
-        //   "#index-slider .list-item:nth-of-type(1) span:nth-of-type(2)"
-        // )
-        //   .text()
-        //   .trim();
         const ul = await page.waitForSelector("#index-slider");
         const li = await ul?.waitForSelector(".list-item");
         const span = await li?.waitForSelector("span");
         const text = await span?.textContent();
         console.log("text:::::::", text);
-        // numericValue = parseFloat(nepseValue.replace(/,/g, ""));
+        nepseValue = parseFloat(text!.replace(/,/g, ""));
+        percentChange = parseFloat(text!.split("  ")[1]);
       },
     });
 
     await crawler.run([nepseUrl]);
-    // console.log(numericValue);
+    console.log(nepseValue, percentChange);
   } catch (error) {
     console.error("Error scraping NEPSE index:", error);
   }
