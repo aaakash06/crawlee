ccf85c89564a5cca09924d92a417bb5e23c99290
aaakash06 <akashbagale182@gmail.com>
2024-12-01 22:51:01 +0545

    playwright

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { CheerioCrawler } from "crawlee";
+import { PlaywrightCrawler } from "crawlee";
 
 export const scrapeNext = async () => {
   try {
     const nepseUrl = "https://merolagani.com/latestmarket.aspx";
     let numericValue: number = 0;
-    const crawler = new CheerioCrawler({
-      async requestHandler({ request, $, _, log }) {
+    const crawler = new PlaywrightCrawler({
+      async requestHandler({ request, page, _, log }) {
         // const nepseValue = $(
         //   "#index-slider .list-item:nth-of-type(1) span:nth-of-type(2)"
         // )
         //   .text()
         //   .trim();
-        const html = $("body").html();
+        const html = await page.textContent("#index-slider");
         console.log("html:::::::", html);
         // numericValue = parseFloat(nepseValue.replace(/,/g, ""));
       },
