cf1cc2c3aaf9537a54726dd03ffd9a4011145a82
aaakash06 <akashbagale182@gmail.com>
2024-12-01 23:00:30 +0545

    playwright working

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,23 @@ export const scrapeNext = async () => {
     const nepseUrl = "https://merolagani.com/latestmarket.aspx";
     let numericValue: number = 0;
     const crawler = new PlaywrightCrawler({
-      async requestHandler({ request, page, _, log }) {
+      async requestHandler({ request, page, enqueueLinks, log }) {
         // const nepseValue = $(
         //   "#index-slider .list-item:nth-of-type(1) span:nth-of-type(2)"
         // )
         //   .text()
         //   .trim();
-        const html = await page.textContent("#index-slider");
-        console.log("html:::::::", html);
+        const ul = await page.waitForSelector("#index-slider");
+        const li = await ul?.waitForSelector(".list-item");
+        const span = await li?.waitForSelector("span");
+        const text = await span?.textContent();
+        console.log("text:::::::", text);
         // numericValue = parseFloat(nepseValue.replace(/,/g, ""));
       },
     });
 
     await crawler.run([nepseUrl]);
-    console.log(numericValue);
+    // console.log(numericValue);
   } catch (error) {
     console.error("Error scraping NEPSE index:", error);
   }
