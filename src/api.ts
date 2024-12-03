9444c204dc7cbbbb780cd1655d8216f26ad48b81
aaakash06 <akashbagale182@gmail.com>
2024-12-03 20:45:33 +0545

    api export

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
+import { scrapeNext } from ".";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get("/", async (req: Request, res: Response) => {
+  const data = await scrapeNext();
+  if (!data) {
+    res.status(500).json({ ok: false, error: "Failed to scrape data" });
+  }
+  res.status(200).json({ ok: true, data });
 });
 
 app.listen(3000, () => {
