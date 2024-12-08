baedf9feeb09801482ad2d7109a9ba7cc6cd2a5b
aaakash06 <akashbagale182@gmail.com>
2024-12-09 04:32:34 +0545

    postman request

--- src/postman.ts
+++ src/postman.ts
@@ -1,5 +1,5 @@
 import request from "request";
-
+import fs from "fs";
 var options = {
   method: "POST",
   url: "https://merolagani.com/signalr/send?transport=serverSentEvents&connectionToken=bBvTL2Jj2Xeurgyqw2iTWSk21dWgojpDxpbMQJrRsBYZCRqPfHdDBmj-Evn8UUtIg9Uwm5CzsOhjgCJxpCo6_nuCPAw8B_QE6fEERBro_Z0qXTwqH0UstsNO64c7rXQFR8bjqNPyxXQYeGjloYmVE98musDzmZiUjO5cyWJn3R4nJUmxLpWRGIrhC0wgw43u0",
@@ -26,5 +26,5 @@ var options = {
 };
 request(options, function (error: any, response: any) {
   if (error) throw new Error(error);
-  console.log(response.body);
+  fs.writeFileSync("stocks.json", response.body);
 });
