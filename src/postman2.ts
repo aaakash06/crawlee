6cc37485d3234fd1f1145968fcf0a978ba4f69e1
aaakash06 <akashbagale182@gmail.com>
2024-12-10 20:53:34 +0545

    summary done

--- src/postman2.ts
+++ src/postman2.ts
@@ -3,6 +3,6 @@ import fs from "fs";
 fetch(
   "https://merolagani.com/handlers/webrequesthandler.ashx?type=market_summary"
 )
-  .then((res) => res.text())
-  .then((data) => fs.writeFileSync("summary.json", data))
+  .then((res) => res.json())
+  .then((data) => fs.writeFileSync("summary.json", JSON.stringify(data)))
   .catch((err) => console.error(err));
