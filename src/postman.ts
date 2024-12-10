3af34d6d69063e412d4a72adeb479f0e703528c8
aaakash06 <akashbagale182@gmail.com>
2024-12-10 20:31:08 +0545

    working

--- src/postman.ts
+++ src/postman.ts
@@ -11,20 +11,11 @@ var options = {
     origin: "https://merolagani.com",
     priority: "u=1, i",
     referer: "https://merolagani.com/latestmarket.aspx",
-    "sec-ch-ua": '"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-    "sec-ch-ua-mobile": "?1",
-    "sec-ch-ua-platform": '"Android"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "sec-gpc": "1",
-    "user-agent":
-      "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36",
-    "x-requested-with": "XMLHttpRequest",
   },
   body: "data=%7B%22H%22%3A%22stocktickerhub%22%2C%22M%22%3A%22GetAllStocks%22%2C%22A%22%3A%5B%5D%2C%22I%22%3A0%7D",
 };
 request(options, function (error: any, response: any) {
   if (error) throw new Error(error);
+  console.log(response.body);
   fs.writeFileSync("stocks.json", response.body);
 });
